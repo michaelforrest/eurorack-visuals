95005eae4ac1b7029ee2c38dee2226c295594920
Michael Forrest <michael.forrest@gmail.com>
2019-02-04 09:57:19 +0000

    version as demoed in the video

--- EuroVizDemo/GameScene.swift
+++ EuroVizDemo/GameScene.swift
@@ -8,82 +8,80 @@
 
 import SpriteKit
 import GameplayKit
+import CoreBluetooth
+
+let FRIENDS = ["🐿", "🦔", "🍂", "🌰", "🥜", "🍃", "🌱", "☘️", "🍀", "🌿"]
 
 class GameScene: SKScene {
+    private var mushroom: SKLabelNode!
+    private var bluetooth: BluetoothSerial!
+    private var centralManager: CBCentralManager!
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+      func addFriend(){
+        let friendElement = SKLabelNode(text: FRIENDS.randomElement())
+        friendElement.position = CGPoint(x:130,y:0)
+        
+        let friend = SKNode()
+        friend.addChild(friendElement)
+        
+        self.addChild(friend)
+        let duration: TimeInterval = 6
+        friendElement.run(SKAction.rotate(byAngle:  .pi * -4, duration: duration))
+        friend.run(SKAction.group([
+            SKAction.rotate(byAngle: .pi * 4, duration: duration),
+            SKAction.sequence([
+                SKAction.wait(forDuration: duration - 1),
+                SKAction.fadeOut(withDuration: 1),
+                SKAction.removeFromParent()
+                ])
+            ]))
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.pulseMushroom()
+        self.addFriend()
+    }
     
     override func didMove(to view: SKView) {
+        self.mushroom = (self.childNode(withName: "//mushroom") as! SKLabelNode)
+        self.bluetooth = BluetoothSerial(delegate: self)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func pulseMushroom(){
+        self.mushroom.run(SKAction(named: "Pulse")!)
+    }
+    
+    
+
+}
+
+extension GameScene: BluetoothSerialDelegate{
+    func serialDidDiscoverPeripheral(_ peripheral: CBPeripheral, RSSI: NSNumber?) {
+        bluetooth.connectToPeripheral(peripheral)
+    }
+    
+    func serialDidReceiveString(_ message: String) {
+        if message == "trigger_1"{
+            self.pulseMushroom()
+            self.addFriend()
         }
+    }
+    
+    func serialDidChangeState() {
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+    }
+    
+    func serialDidDisconnect(_ peripheral: CBPeripheral, error: NSError?) {
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    }
+ 
+}
+
+extension GameScene: CBCentralManagerDelegate{
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if(central.state == .poweredOn){
+            bluetooth.startScan()
         }
-    }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
     }
 }
